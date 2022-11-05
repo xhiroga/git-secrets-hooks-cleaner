8b4dba86614a40a98bb0156f5e2cb50382b67bf6
xhiroga <13391129+xhiroga@users.noreply.github.com>
2022-11-05 13:20:07 +0900

    feat: show results

--- cmd/list.go
+++ cmd/list.go
@@ -4,9 +4,12 @@ Copyright © 2022 NAME HERE <EMAIL ADDRESS>
 package cmd
 
 import (
+	"fmt"
 	"os"
 	"path/filepath"
+	"strings"
 
+	"github.com/fatih/color"
 	"github.com/spf13/cobra"
 	"go.uber.org/zap"
 )
@@ -20,6 +23,14 @@ func getLogger(verbose bool) *zap.Logger {
 	return logger
 }
 
+type UseGitSecrets int
+
+const (
+	No UseGitSecrets = iota
+	Modified
+	TemplateCompatible
+)
+
 var commitMsgTemplate = `#!/usr/bin/env bash
 git secrets --commit_msg_hook -- "$@"
 `
@@ -30,12 +41,7 @@ var prepareCommitMsg = `#!/usr/bin/env bash
 git secrets --prepare_commit_msg_hook -- "$@"
 `
 
-func isFromTemplate(path string) (bool, error) {
-	bytes, err := os.ReadFile(path)
-	if err != nil {
-		return false, err
-	}
-	content := string(bytes)
+func isTemplateCompatible(path string, content string) (bool, error) {
 	switch filepath.Base(path) {
 	case "commit-msg":
 		return content == commitMsgTemplate, nil
@@ -45,6 +51,74 @@ func isFromTemplate(path string) (bool, error) {
 		return content == prepareCommitMsg, nil
 	}
 	return false, nil
+}
+func doesUseGitSecrets(path string) (UseGitSecrets, error) {
+	bytes, err := os.ReadFile(path)
+	if err != nil {
+		return No, err
+	}
+	content := string(bytes)
+	compatible, err := isTemplateCompatible(path, content)
+	if err != nil {
+		return No, err
+	}
+	if compatible {
+		return TemplateCompatible, nil
+	}
+	if strings.Contains(content, "git secrets") {
+		return Modified, nil
+	}
+	return No, nil
+}
+
+type hook struct {
+	path          string
+	useGitSecrets UseGitSecrets
+}
+
+func getHooks(baseDir string, sugar *zap.SugaredLogger) ([]hook, error) {
+	var hooks []hook
+	// filepath.Glob doesn't support **, so we need to walk the directory
+	// https://stackoverflow.com/a/26809999/7869792
+	err := filepath.WalkDir(baseDir, func(path string, d os.DirEntry, err error) error {
+		if err != nil {
+			return err
+		}
+		if d.IsDir() {
+			return nil
+		}
+		if filepath.Base(filepath.Dir(path)) == "hooks" {
+			sugar.Debugf("Found file: %s", path)
+			useGitSecrets, err := doesUseGitSecrets(path)
+			if err != nil {
+				return err
+			}
+			sugar.Debugf("Use git-secrets: %v", useGitSecrets)
+			hooks = append(hooks, hook{path: path, useGitSecrets: useGitSecrets})
+		}
+		return nil
+	})
+	if err != nil {
+		sugar.Fatalf("Error walking directory: %s", err)
+		return nil, err
+	}
+	return hooks, nil
+}
+
+func showHooks(hooks []hook) {
+	fmt.Println("Path\tUse git-secrets")
+	fmt.Println("----\t--------------")
+	for _, hook := range hooks {
+		fmt.Printf("%s\t", hook.path)
+		switch hook.useGitSecrets {
+		case No:
+			color.Green("No")
+		case Modified:
+			color.Yellow("Modified")
+		case TemplateCompatible:
+			color.Red("Template compatible")
+		}
+	}
 }
 
 // listCmd represents the list command
@@ -68,31 +142,11 @@ var listCmd = &cobra.Command{
 			baseDir = currentDir
 		}
 
-		// filepath.Glob doesn't support **, so we need to walk the directory
-		// https://stackoverflow.com/a/26809999/7869792
-
-		var files []string
-		err := filepath.WalkDir(baseDir, func(path string, d os.DirEntry, err error) error {
-			if err != nil {
-				return err
-			}
-			if d.IsDir() {
-				return nil
-			}
-			if filepath.Base(filepath.Dir(path)) == "hooks" {
-				sugar.Debugf("Found file: %s", path)
-				fromTemplate, err := isFromTemplate(path)
-				if err != nil {
-					return err
-				}
-				sugar.Debugf("From template: %v", fromTemplate)
-				files = append(files, path)
-			}
-			return nil
-		})
+		hooks, err := getHooks(baseDir, sugar)
 		if err != nil {
-			sugar.Fatalf("Error walking directory: %s", err)
+			sugar.Fatalf("Error getting hooks: %s", err)
 		}
+		showHooks(hooks)
 	},
 }
 
