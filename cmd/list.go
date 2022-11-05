d5e27108606ef8f86c0619ccf1750fd1dc1d4070
xhiroga <13391129+xhiroga@users.noreply.github.com>
2022-11-05 12:10:47 +0900

    chore: add logger

--- cmd/list.go
+++ cmd/list.go
@@ -9,35 +9,47 @@ import (
 	"path/filepath"
 
 	"github.com/spf13/cobra"
+	"go.uber.org/zap"
 )
+
+func getLogger(verbose bool) *zap.Logger {
+	if verbose {
+		logger, _ := zap.NewDevelopment()
+		return logger
+	}
+	logger, _ := zap.NewProduction()
+	return logger
+}
 
 // listCmd represents the list command
 var listCmd = &cobra.Command{
 	Use:   "list",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "",
+	Long:  ``,
 	Run: func(cmd *cobra.Command, args []string) {
-		currentDir, _ := os.Getwd()
-		matches, _ := filepath.Glob(currentDir + "/**/.git/hooks/*")
-		fmt.Println(matches)
+		verbose, _ := cmd.Flags().GetBool("verbose")
+		fmt.Println(verbose)
+		logger := getLogger(verbose)
+		defer logger.Sync() // flushes buffer, if any
+		sugar := logger.Sugar()
+
+		baseDir, _ := cmd.Flags().GetString("base-dir")
+		sugar.Debugf("Base dir: %s", baseDir)
+
+		if baseDir == "" {
+			currentDir, _ := os.Getwd()
+			sugar.Debugf("Current dir: %s", currentDir)
+			baseDir = currentDir
+		}
+
+		matches, _ := filepath.Glob(baseDir + "/**/.git/hooks/*")
+		sugar.Debugf("Found %d hooks", len(matches))
+		sugar.Debug(matches)
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(listCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// listCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// listCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	listCmd.Flags().String("base-dir", "", "Base directory to search for hooks")
+	listCmd.Flags().Bool("verbose", false, "Verbose output")
 }
--- go.mod
+++ go.mod
@@ -6,4 +6,7 @@ require (
 	github.com/inconshreveable/mousetrap v1.0.1 // indirect
 	github.com/spf13/cobra v1.6.1 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
+	go.uber.org/atomic v1.10.0 // indirect
+	go.uber.org/multierr v1.8.0 // indirect
+	go.uber.org/zap v1.23.0 // indirect
 )
